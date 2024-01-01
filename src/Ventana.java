eec37a337ae5a8add503b4f0132ce6f5d67b0785
LorenaAlbillos <lalbim00@estudiantes.unileon.es>
2024-01-01 22:57:07 +0100

    Modificaciones clase Ventana

--- src/Ventana.java
+++ src/Ventana.java
@@ -4,7 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -26,7 +28,7 @@ public class Ventana extends JFrame implements ActionListener{
 	private JMenuBar barra;
 	private JLabel titulo;
 	private JPanel panel1;
-	private static JPanel panel2;
+	private JPanel panel2;
 	private JPanel panel3;
 	
 	private JMenu archivo;
@@ -115,6 +117,7 @@ public class Ventana extends JFrame implements ActionListener{
 		this.crear.addActionListener(this);
 		this.guardar.addActionListener(this);
 		this.guardarComo.addActionListener(this);
+		this.cargar.addActionListener(this);
 		
 		tablero = new ArrayList<ArrayList<Ficha>>();
 		
@@ -165,6 +168,10 @@ public class Ventana extends JFrame implements ActionListener{
 		
 		if(this.guardarComo == e.getSource()) {
 			guardarComo();
+		}
+		
+		if(this.cargar == e.getSource()) {
+			cargar();
 		}
 	}
 	
@@ -229,7 +236,7 @@ public class Ventana extends JFrame implements ActionListener{
 						
 						//Si no hay nada o hay algo que no sea uno de los 3 colores se sustituye por VACIO
 					default: 
-							 JOptionPane.showMessageDialog(getParent(), "Error en la posicion ( " + fila + "," + columna + " )");
+							 //JOptionPane.showMessageDialog(getParent(), "Error en la posicion ( " + fila + "," + columna + " )");
 							 tablero.get(fila).get(columna).setColor(Fichas.Colores.VACIO);
 							 text.setText("");
 						break;
@@ -319,7 +326,7 @@ public class Ventana extends JFrame implements ActionListener{
 			 * -> -1 si se ha cancelado*/
 			int status = archivo.showSaveDialog(null);
 			
-			/*Si se aceptase seleccionael file que se va a guardar
+			/*Si se aceptase seleccionar el file que se va a guardar
 			 * con getSelectedFile()*/
 			if(status == 0) {
 				file = archivo.getSelectedFile();
@@ -359,9 +366,148 @@ public class Ventana extends JFrame implements ActionListener{
 	private void cargar() {
 		JFileChooser archivo = new JFileChooser();
 		
+		//con el metodo showOpenDialog(null) escogemos el archivo que queramos leer
+		int status = archivo.showOpenDialog(null);
+		
+		/*Comprobamos que se haya presionado en confirmar
+		 * y seleccionamos nuestro archivo*/	
+		if(status == 0) {
+			this.file = archivo.getSelectedFile();
+		} else {
+			return;
+		}
+		
+		try {
+			this.ruta = this.file.getAbsolutePath();
+			
+			/*Comprobar que el archivo sea correcto*/
+			File aux = new File(this.ruta);
+			if(!aux.exists()) {
+				JOptionPane.showMessageDialog(this, "No existe el fichero como para poder cargarlo.", null, JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+				
+			if(!this.ruta.endsWith(".txt")) {
+				JOptionPane.showMessageDialog(this, "Este tipo de archivo no se puede cargar..", null, JOptionPane.ERROR_MESSAGE);
+				return;	
+			}
+			
+			/*Creamos una hoja de lectura pasandole el archivo obtenido*/
+			FileReader lectura = new FileReader(this.file);
+			
+			/*Crear bufferedReader al que le pasaremos la hoja*/
+			BufferedReader buffer = new BufferedReader(lectura);
+			
+			/*Crear una lista para guardar las lineas leidas del archivo*/
+			ArrayList<String> lista = new ArrayList<String>();
+			
+			/*Leeremos todas las lineas con el metodo readLine y añadirlas a la lista*/
+			String linea = buffer.readLine();
+			while(linea != null) {
+				lista.add(linea);
+				linea = buffer.readLine();
+			}
+			
+			/*Comprobar que el numero de lineas sea >0*/
+			if(lista.size() <= 0) {return;}
+			
+			/*Comprobar que el numero de lineas sea <= 20*/
+			if(lista.size() > 20) {return;}
+			
+			for(int i = 0; i < lista.size(); i++) {
+				/*Comprobar que los caracteres de una lina sean > 0 && <= 20*/
+				if(lista.get(i).length()  <= 0 || lista.get(i).length() > 20) {
+					JOptionPane.showMessageDialog(this, "Error al cargar el archivo 1.", null, JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				
+				/*Comprobar que hay la misma cantidad de caracteres por fila*/
+				if(i != 0 && lista.get(i).length() != lista.get(i - 1).length()) {
+					JOptionPane.showMessageDialog(this, "Error al cargar el archivo 2.", null, JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				
+				for(int j = 0; j < lista.get(i).length(); j++) {
+					/*Recorrer todas las lineas y analizar si contienen R, V, A
+					 * -> si alguna no lo contiene se saca error y salimos*/
+					if(lista.get(i).charAt(j) != 'R'  && lista.get(i).charAt(j) != 'A' && lista.get(i).charAt(j) != 'V'){
+						JOptionPane.showMessageDialog(this, "Error al cargar el archivo 3.", null, JOptionPane.ERROR_MESSAGE);
+						return;
+					}	
+				}
+			}
+			guardarTablero(lista);
+			cargarTablero();
+			
+		} catch(Exception exception) {
+			JOptionPane.showMessageDialog(this, "Error al cargar el archivo 4.", null, JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		
+	}
+	
+	private void guardarTablero(ArrayList<String> lista) {
+		this.tablero= new ArrayList<>();
+		for(int i = 0; i< lista.size(); i++) {
+			ArrayList<Ficha> fila = new ArrayList<Ficha>();
+			for(int j = 0; j < lista.get(i).length(); j++) {
+				Ficha ficha;
+				switch(lista.get(i).charAt(j)) {
+					case 'A': ficha = new Ficha(Fichas.Colores.A, new Posicion(i, j));
+							  fila.add(ficha);
+						break;
+					case 'R': ficha = new Ficha(Fichas.Colores.R, new Posicion(i, j));
+							  fila.add(ficha);
+						break;
+					case 'V': ficha = new Ficha(Fichas.Colores.V, new Posicion(i, j));
+							  fila.add(ficha);
+						break;
+				}
+			}
+			this.tablero.add(fila);
+		}
+	}
+	
+	private void cargarTablero() {
+		/*Elimino todo del panel2*/
+		this.panel2.removeAll();
+		
+		//actualizo tablero
+		this.setVisible(true);
+		
+		/*Definir nuevo gridLayout con las nuevas filas y columnas*/
+		GridLayout grid = new GridLayout(this.tablero.size(), this.tablero.get(0).size());
+		this.panel2.setLayout(grid);
+		
+		/*Para cada elemento, mostramos en el JTextField su contenido*/
+		for(int i = 0; i < this.tablero.size(); i++) {
+			for(int j = 0; j < this.tablero.get(0).size(); j++) {
+				JTextField text = new JTextField();
+				
+				if(!this.tablero.get(i).get(j).getColor().equals(Colores.VACIO)) {
+					text.setText(this.tablero.get(i).get(j).getColor() + " ");
+				}
+				/*Añadir el addFocus*/
+				addFocus(text, i, j);
+				this.panel2.add(text);
+			}
+		}
+		
+		
+		/*Recargar la lista*/
+		this.setVisible(true);
 	}
 	
 	enum Colores{
         R, V, A, VACIO;
     }
+}
+
+private class BotonJuego extends JButton implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
